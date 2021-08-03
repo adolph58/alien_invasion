710d8bffce761b34bbf858da827340485776c97f
Arte <jiangtao.cheng@orderplus.com>
2021-08-03 15:25:41 +0800

    1.q 退出 2.全屏

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,7 +13,14 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        # 独立窗口运行
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        # 全屏运行
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         # 设置背景色。
@@ -33,18 +40,28 @@ class AlienInvasion:
         """响应按键和鼠标事件。"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exti()
+                sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # 向右移动飞船。
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按键。"""
+        if event.key == pygame.K_RIGHT:
+            # 向右移动飞船。
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应松开"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕。"""
