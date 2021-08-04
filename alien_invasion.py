9e9aca7c67747b28a9826d6e204c78467ab4c2b7
Arte <jiangtao.cheng@orderplus.com>
2021-08-04 15:40:29 +0800

    1.删除消失的子弹 2.限制子弹数量

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-
 
     def _check_events(self):
         """响应按键和鼠标事件。"""
@@ -69,9 +68,20 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        """创建一颗子弹"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        """创建新子弹并将其加入编组 bullets 中。"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹。"""
+        # 更新子弹的位置。
+        self.bullets.update()
+
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕。"""
